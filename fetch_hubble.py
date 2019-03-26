259d90f0dcd1a16b60c13964e3eabd2b303573be
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-03-26 10:35:48 +0300

    Привели в порядок
    Убрал папку images
    Вместо одного скрипта - три разных файла:
    fetch_spacex.py -для скачивания последних фотографий SpaceX
    fetch_Hubble.py - для скачивания коллекции spacecraft
    space_as_space.py - для выкладывания картинок в инстаграмм

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,35 +1,24 @@
 import requests
 import os
 
-
 def get_extension_file(name):
-    '''Возвращает расширение файла'''
+    '''Get extension of 'name' file'''
     return name.split('.')[-1]
 
-
 def download_picture(pic_path, pic_name, pic_dir):
+    '''Download picture by 'pic_path' address, 'pic_name' name to 'pic_dir' folder'''
     if not os.path.exists(pic_dir):
         os.makedirs(pic_dir)
 
     filename = os.path.join(pic_dir, pic_name)
-
     response = requests.get(pic_path)
     with open(filename, 'wb') as file:
         file.write(response.content)
     return True
 
-
-def fetch_spacex_last_lunch():
-    response = requests.get('https://api.spacexdata.com/v3/launches/latest')
-
-    list_of_pic = response.json()['links']['flickr_images']
-
-    for pic_number, pic in enumerate(list_of_pic):
-        download_picture(pic, 'spacex'+str(pic_number)+'.jpg', 'images')
-    return True
-
-
 def get_Hubble_image_by_id(id):
+    '''Download picture with 'id' to 'pic_dir' folder '''
+
     response = requests.get('http://hubblesite.org/api/v3/image/'+str(id))
 
     pic_path = response.json()['image_files'][-1]['file_url']
@@ -41,16 +30,14 @@ def get_Hubble_image_by_id(id):
     return True
 
 def get_Hubble_image_collection(collection):
+    '''Get list of Habble telescope picture's id with 'colelction' mark '''
 
     response = requests.get('http://hubblesite.org/api/v3/images?page=all&collection_name='+collection)
-
     images_collection = response.json()
 
     return [images_collection[i]['id'] for i in range(len(images_collection))]
 
-
 if __name__ == '__main__':
-
     list_of_pic_id = get_Hubble_image_collection('spacecraft')
     for pic_id in list_of_pic_id:
         get_Hubble_image_by_id(pic_id)
---
+++ fetch_spacex.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+def get_extension_file(name):
+    '''Возвращает расширение файла'''
+    return name.split('.')[-1]
+
+def download_picture(pic_path, pic_name, pic_dir):
+    '''Скачивает картинку с адресом pic_path, с именем pic_name в директорию pic_dir '''
+    if not os.path.exists(pic_dir):
+        os.makedirs(pic_dir)
+
+    filename = os.path.join(pic_dir, pic_name)
+
+    response = requests.get(pic_path)
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    return True
+
+def fetch_spacex_last_launch():
+    response = requests.get('https://api.spacexdata.com/v3/launches/latest')
+
+    list_of_pic = response.json()['links']['flickr_images']
+
+    for pic_number, pic in enumerate(list_of_pic):
+        download_picture(pic, 'spacex'+str(pic_number)+'.jpg', 'images')
+    return True
+
+if __name__ == '__main__':
+    fetch_spacex_last_launch()
