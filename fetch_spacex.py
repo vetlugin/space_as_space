60c31461136b9dd5da76a066c7a123d4ae0cae54
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-05-04 10:33:52 +0300

    Add datetime mark in filename

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,13 +1,17 @@
 import requests
 import os
 import download_picture as dp
+import datetime
+
 
 def fetch_spacex_last_launch():
+    todaydate = datetime.datetime.today().strftime("%Y%m%d")
+
     response = requests.get('https://api.spacexdata.com/v3/launches/latest')
     list_of_pic = response.json()['links']['flickr_images']
 
     for pic_number, pic in enumerate(list_of_pic):
-        dp.download_picture(pic, 'spacex{}.jpg'.format(pic_number), 'images')
+        dp.download_picture(pic, f'spacex{todaydate}-{pic_number}.jpg', 'images')
 
 if __name__ == '__main__':
     fetch_spacex_last_launch()
