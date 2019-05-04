437352f9dc226ecd7004f61c6a3209d3e2c32c19
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-05-05 00:21:55 +0300

    Добавил скачку патча и название миссии в названии файлов

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,13 +5,14 @@ import datetime
 
 
 def fetch_spacex_last_launch():
-    todaydate = datetime.datetime.today().strftime("%Y%m%d")
-
     response = requests.get('https://api.spacexdata.com/v3/launches/latest')
     list_of_pic = response.json()['links']['flickr_images']
+    mission_name = response.json()['mission_name']
+    mission_patch_path = response.json()['links']['mission_patch']
 
+    dp.download_picture(mission_patch_path, f'spacex{mission_name}-mission_patch.jpg', 'images')
     for pic_number, pic in enumerate(list_of_pic):
-        dp.download_picture(pic, f'spacex{todaydate}-{pic_number}.jpg', 'images')
+        dp.download_picture(pic, f'spacex{mission_name}-{pic_number}.jpg', 'images')
 
 if __name__ == '__main__':
     fetch_spacex_last_launch()
